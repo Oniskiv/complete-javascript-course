d28071e65b54d05369d64568ef82e583ffedafee
Vladislav Oniskiv <onisckif.vladislaw@mail.ru>
2025-01-28 20:19:58 +0400

    Module 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function logger() {
     console.log("Hello World!");
 }
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+function process(name) {
+  debugger;
+  return `My name is ${name}`;
+}
+console.log(process('Alex'));
