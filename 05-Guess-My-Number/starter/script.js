3c597ec260c75b6b0c0ad75c46f038294fc576bf
Uladzislau Onyskiu <uonyskiu@lenovo.com>
2025-01-29 18:47:28 +0400

    Module 5

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/*
+console.log(document.querySelector(".message").textContent);
+
+console.log(document.querySelector(".number").textContent);
+document.querySelector(".number").textContent = 14;
+console.log(document.querySelector(".number").textContent);
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23.
+console.log(document.querySelector(".guess").value);
+*/
+
