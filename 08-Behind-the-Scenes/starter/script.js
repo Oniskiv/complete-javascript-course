e7f95c8d36d95a3a744a6fbc397a7a0fdf9bcbc7
Uladzislau Onyskiu <uonyskiu@lenovo.com>
2025-02-03 20:00:40 +0400

    Module 8

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,21 @@
-'use strict';
+'use strict'
+
+function test() {
+  const age = 25;
+  console.log(age);
+
+  if (age === 25) {
+    const name = "name";
+    var surname = "surname";
+
+    function say() {
+      console.log(`Age is ${age}`);
+    }
+  }
+
+  //console.log(name);
+  console.log(surname);
+  //say();
+}
+
+test();
