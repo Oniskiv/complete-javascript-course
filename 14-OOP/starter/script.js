8631aeb11a6305cf4b56c7dad188bdd38622aac3
Vladislav Oniskiv <onisckif.vladislaw@mail.ru>
2025-03-22 15:00:00 +0400

    Module 14

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,54 @@
-'use strict';
+"use strict";
+
+const Person = function(firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function() {
+  console.log(2025 - this.birthYear);
+}
+Person.hey = function() {
+  console.log("Hey");
+}
+
+const alex = new Person("Alex", 1990);
+console.log(alex);
+alex.calcAge();
+
+Person.hey();
+//alex.hey();
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+   this.firstName = firstName;
+   this.birthYear = birthYear;
+  };
+
+  calcAge() {
+    console.log(2025 - this.birthYear);
+  };
+
+  get age() {
+    return 2025 - this.birthYear;
+  }
+
+  set data(data) {
+    this._data = data;
+  }
+
+  static hey() {
+    console.log("Hey");
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1980);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+jessica.data = "Hello";
+console.log(jessica._data);
+
+PersonCl.hey();
+//jessica.hey();
