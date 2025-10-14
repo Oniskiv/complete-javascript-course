87e9df6505083903f71286db481733315d3b4d31
Vladislav Oniskiv <onisckif.vladislaw@mail.ru>
2025-10-14 21:37:35 +0300

    Module 18

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -36,17 +37,25 @@ const controlSearchResults = async function() {
 
     await model.loadSearchResults(query);
 
-    console.log(model.state.search.results);
-    resultsView.render(model.state.search.results);
+    console.log(model.getSearchResultPage());
+    resultsView.render(model.getSearchResultPage());
+    paginationView.render(model.state.search);
   } catch (err) {
     console.error(err);
     recipeView.renderError();
   }
 };
 
+const controlPagination = function(goToPage) {
+  console.log(goToPage);
+  resultsView.render(model.getSearchResultPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
